175440acfe5b4d7d35ce9a71b846a3c7d7e2651d
Chamath Jayasekara <chamathjayasekara5962@gmail.com>
2022-05-02 09:59:42 +0530

    user data retrival
    data retrieval using MVVM is done

--- app/src/main/java/com/example/aale/activities/RegisterActivity.java
+++ app/src/main/java/com/example/aale/activities/RegisterActivity.java
@@ -4,7 +4,6 @@ package com.example.aale.activities;
 
 
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -19,7 +18,6 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -27,9 +25,8 @@ import android.widget.Toast;
 import com.example.aale.R;
 import com.example.aale.databinding.ActivityRegisterBinding;
 import com.example.aale.model.Customer;
-import com.example.aale.model.User;
 import com.example.aale.model.UserFactory;
-import com.example.aale.utils.DBConnectionUtil;
+import com.example.aale.repo.DBConnectionRepository;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -38,7 +35,6 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 
 public class RegisterActivity extends AppCompatActivity {
@@ -67,7 +63,7 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         getSupportActionBar().hide();
         //intializing database
-        dbRef= DBConnectionUtil.getCustomerReference();
+        dbRef= DBConnectionRepository.getCustomerReference();
 
         mAuth=FirebaseAuth.getInstance();
         //If the user is already logged
--- app/src/main/java/com/example/aale/ui/users/UsersFragment.java
+++ app/src/main/java/com/example/aale/ui/users/UsersFragment.java
@@ -1,5 +1,6 @@
 package com.example.aale.ui.users;
 
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
@@ -8,6 +9,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,22 +18,41 @@ import android.widget.TextView;
 import com.example.aale.R;
 import com.example.aale.databinding.FragmentSwipesBinding;
 import com.example.aale.databinding.FragmentUsersBinding;
+import com.example.aale.model.Customer;
 import com.example.aale.ui.swipes.SwipesViewModel;
+
+import java.util.List;
 
 public class UsersFragment extends Fragment {
     private FragmentUsersBinding binding;
-
+    private  UsersViewModel usersViewModel;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        UsersViewModel usersViewModel = new ViewModelProvider(this).get(UsersViewModel.class);
+
         binding =  FragmentUsersBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-       // usersViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        usersViewModel = new ViewModelProvider(this).get(UsersViewModel.class);
+
+        usersViewModel.getUsers().observe(getViewLifecycleOwner(), new Observer<List<Customer>>() {
+            @Override
+            public void onChanged(List<Customer> customers) {
+              for(Customer customer : customers){
+
+              }
+
+            }
+        });
+    }
+
     @Override
     public void onDestroyView() {
         super.onDestroyView();
