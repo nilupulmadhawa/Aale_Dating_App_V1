dddf6dd28a8ddef6dec446753450e5cc8673926d
AnuradhaDiyu <it20662264@my.sliit.lk>
2022-05-06 23:17:46 +0530

    Consent part added.

---
+++ app/src/main/java/com/example/aale/Conscent.java
@@ -0,0 +1,14 @@
+package com.example.aale;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Conscent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conscent);
+    }
+}
