da034eb2f5bc63c6a8d507da93ca5ef28cd2d54a
Chamath Jayasekara <chamathjayasekara5962@gmail.com>
2022-05-06 20:13:06 +0530

    Bottom Navigation Added

---
+++ app/src/main/java/com/example/aale/ui/chat_bubble/ChatBubbleFragment.java
@@ -0,0 +1,29 @@
+package com.example.aale.ui.chat_bubble;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.aale.R;
+
+
+public class ChatBubbleFragment extends Fragment {
+
+
+
+    public ChatBubbleFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chat_bubble, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/aale/ui/find_lover/FindLoverFragment.java
@@ -0,0 +1,27 @@
+package com.example.aale.ui.find_lover;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.aale.R;
+
+
+public class FindLoverFragment extends Fragment {
+
+    public FindLoverFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_find_lover, container, false);
+    }
+}
